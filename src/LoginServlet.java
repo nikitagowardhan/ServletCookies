058149fc7bd3d4fbe60b1e7b07130b38892a4e63
nikitagowardhan <nikitagowar02@gmail.com>
2024-01-11 01:12:29 +0530

    ServletCookies

---
+++ src/LoginServlet.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.Cookies;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	      response.setContentType("text/html");
+	      PrintWriter p = response.getWriter();
+	      
+	      request.getRequestDispatcher("link.html").include(request, response); 
+	      
+	      String name = request.getParameter("uname");
+	      String pass = request.getParameter("upass");
+	      
+	     
+	      
+	      if(pass.equals("admin"))
+	      {
+	    	 p.print("You are successfully logged in <br>");
+	    	 p.print("Welcome "+name );
+	    	 
+	    	Cookie c1 = new Cookie("uname", name);
+	    	response.addCookie(c1);
+	    	
+	      }
+
+	      else
+	      {
+	    	  p.print("sorry, username or password error!");
+	    	  request.getRequestDispatcher("Login.html").forward(request, response);
+	      }
+	
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/ProfileServlet.java
@@ -0,0 +1,47 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ProfileServlet")
+public class ProfileServlet extends HttpServlet {
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.setContentType("text/html");  
+        PrintWriter p=response.getWriter(); 
+        
+          
+        request.getRequestDispatcher("link.html").include(request, response); 
+        
+        Cookie c[]= request.getCookies();
+        if(c!=null){
+        String name = c[0].getValue();
+        
+        if(!name.equals("")||name!=null){  
+            p.print("<br><b>Welcome to Profile</b>");  
+            p.print("<br>Welcome, "+name);  
+        }  
+        }
+        else{  
+            p.print("Please login first");  
+            request.getRequestDispatcher("login.html").include(request, response);  
+        }  
+
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
